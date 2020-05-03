ac286f2a41328898c9765013639fa2363fc7275a
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2020-05-03 13:57:27 +0900

    依存ライブラリを追加、ライセンスを追加、クラスの雛形を追加

---
+++ VMCProtocolMultiplexer/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMCProtocolMultiplexer
+{
+    class Main
+    {
+        public void Process()
+        {
+            Console.WriteLine("Press ENTER key to stop server");
+            Console.ReadLine();
+        }
+    }
+}
